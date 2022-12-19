c0079f5196adf6d3e2a010c7830d41e1d9b09655
Josh Keller <jrkeller@gmail.com>
2022-12-19 11:17:11 -0500

    2022 day15

--- 2022/src/day15/index.ts
+++ 2022/src/day15/index.ts
@@ -1,4 +1,10 @@
 import run from "aocrunner"
+import { Range } from "../utils/Range2.js"
+
+const onlyTests = false
+
+const TARGET_ROW_Y = onlyTests ? 10 : 2000000
+const MAX_COORD = onlyTests ? 20 : 4000000
 
 const parseInput = (rawInput: string) => {
   const lines = rawInput.split("\n")
@@ -37,27 +43,8 @@ const manhattanDist = (p1: Point, p2: Point): number =>
   Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y)
 
 const part1 = (rawInput: string) => {
-  // const TARGET_ROW_Y = 10
-  const TARGET_ROW_Y = 2000000
   const sbPairsByRow = parseInput(rawInput)
-  let minx = Infinity,
-    maxx = -Infinity,
-    miny = Infinity,
-    maxy = -Infinity
-  let maxDist = 0
-
-  Object.values(sbPairsByRow).forEach((row) => {
-    row.forEach(({ s, b }) => {
-      minx = Math.min(minx, s.x, b.x)
-      miny = Math.min(miny, s.y, b.y)
-      maxx = Math.max(maxx, s.x, b.x)
-      maxy = Math.max(maxy, s.y, b.y)
-      maxDist = Math.max(maxDist, manhattanDist(s, b))
-    })
-  })
-
-  // const targetRow: {[x: number]: 'B'|'#' } = {}
-  const targetRow = new Set<number>()
+  let targetRow: Range[] = []
 
   for (const row in sbPairsByRow) {
     const rowPairs = sbPairsByRow[row]
@@ -66,38 +53,97 @@ const part1 = (rawInput: string) => {
       const distToTargetRow = Math.abs(TARGET_ROW_Y - s.y)
       const reachForThisRow = sensorReach - distToTargetRow
 
-      // console.log("s:", s, "b:", b)
-      // console.log("sensorReach:", sensorReach, "distToTarget:", distToTargetRow)
-      // console.log("rowReach:", reachForThisRow)
-      //
-      if (b.y === TARGET_ROW_Y) {
-        targetRow.add(b.x)
-        // targetRow[b.x] = "B"
-      }
-
       // Skip if everything if the sensor can't reach
       if (distToTargetRow > sensorReach) {
         continue
       }
 
-      for (let x = s.x - reachForThisRow; x <= s.x + reachForThisRow; x++) {
-        // Skip beacon in the target row
-        if (x === b.x) {
-          continue
+      // Make a range from s.x-reachForThisRow - s.x + reachForThisRow
+      const sensorRowRange = new Range(
+        s.x - reachForThisRow,
+        s.x + reachForThisRow,
+      )
+
+      // Add sensor ranges to the target row, skipping beacons
+      if (b.y !== TARGET_ROW_Y || !sensorRowRange.contains(b.x)) {
+        targetRow = addRangeToList(targetRow, sensorRowRange)
+      } else {
+        if (s.x - reachForThisRow !== b.x) {
+          const newRange = new Range(s.x - reachForThisRow, b.x - 1)
+          targetRow = addRangeToList(targetRow, newRange)
         }
-        targetRow.add(x)
+        if (s.x + reachForThisRow !== b.x) {
+          const newRange = new Range(b.x + 1, s.x + reachForThisRow)
+          targetRow = addRangeToList(targetRow, newRange)
+        }
       }
     }
   }
 
-  // console.log("Target Row:", targetRow)
-  return targetRow.size
+  return targetRow.reduce((sum, range) => sum + range.size(), 0)
+}
+
+const addRangeToList = (list: Range[], range: Range): Range[] => {
+  const sortedList = list.slice().sort((a, b) => a.start - b.start)
+  const newList: Range[] = []
+  let currRange = range
+
+  for (const rangeFromList of sortedList) {
+    const maybeJoined = currRange.add(rangeFromList)
+
+    if (Array.isArray(maybeJoined)) {
+      let rangeToPush
+      ;[rangeToPush, currRange] = maybeJoined
+      newList.push(rangeToPush)
+    } else {
+      currRange = maybeJoined
+    }
+  }
+
+  newList.push(currRange)
+
+  return newList
 }
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput)
+  const sbPairsByRow = parseInput(rawInput)
+  const sensorCoverage: { [y: number]: Range[] } = {}
 
-  return
+  for (const row in sbPairsByRow) {
+    const rowPairs = sbPairsByRow[row]
+    for (const { s, b } of rowPairs) {
+      // console.log("s:", s, "b:", b)
+      const sensorReach = manhattanDist(s, b)
+      const y_start = Math.max(0, s.y - sensorReach)
+      const y_end = Math.min(MAX_COORD, s.y + sensorReach)
+      for (let y = y_start; y <= y_end; y++) {
+        const distToTargetRow = Math.abs(y - s.y)
+        const reachForThisRow = sensorReach - distToTargetRow
+
+        const sensorRowRange = new Range(
+          Math.max(s.x - reachForThisRow, 0),
+          Math.min(s.x + reachForThisRow, MAX_COORD),
+        )
+
+        // console.log("y:", y, "Adding:", sensorRowRange, "to", sensorCoverage[y])
+        sensorCoverage[y] = addRangeToList(
+          sensorCoverage[y] || [],
+          sensorRowRange,
+        )
+      }
+      // console.log("Result:", sensorCoverage)
+    }
+  }
+
+  // console.log(sensorCoverage)
+
+  for (let y = 0; y <= MAX_COORD; y++) {
+    if (sensorCoverage[y].length !== 1) {
+      return (sensorCoverage[y][0].end + 1) * 4000000 + y
+    }
+  }
+
+  throw new Error("got to end")
 }
 
 run({
@@ -144,11 +190,11 @@ Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
 Sensor at x=20, y=1: closest beacon is at x=15, y=3
         `,
-        expected: "",
+        expected: 56000011,
       },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: false,
+  onlyTests: onlyTests,
 })
