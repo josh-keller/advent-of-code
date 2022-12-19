1f34c8cf188d96f8154a1070097037b525d1b5ed
Josh Keller <jrkeller@gmail.com>
2022-12-19 11:16:22 -0500

    2022 day 14

--- 2022/src/day14/index.ts
+++ 2022/src/day14/index.ts
@@ -1,48 +1,5 @@
 import run from "aocrunner"
 
-// Data:
-// 2d array?
-// 2d object?
-// col: {
-// 6: {
-//   498:
-// }
-// }
-// Sand falls in:
-//   - goes down until there is soemthing blocking (next highest y coord)
-//     - if there is nothing blocking, we are done
-//   - once something is blocking, it looks at x-1, y+1
-//     - if that is free, start over again above
-//   - else look x+1, y+1
-//     - if that is free, start over again above
-//   - else, rest
-//     - mark that grain of sand on the grid
-//     - increment resting sands
-//
-// resting_sand = 0
-// LOOP:
-//   - while true
-//     - new grain of sand at starting ;point
-//     - resting = false
-//     - while sand is not resting
-//       - go to last available x in the current column
-//       - if there is no last available column:
-//         - return resting_sand
-//       - if down and left is free:
-//         - change to that position and go to top of loop
-//       - if down and right is free:
-//         - change to that position and go to top of loop
-//       - else
-//         - resting = true
-//         - increment resting_sand
-//         - mark resting_sand on the grid
-//         - break inner loop
-
-// columns = {
-//   496: ['.', '.', '.']
-//   etc...
-// }
-
 type Marker = "." | "o" | "#"
 
 class Point {
@@ -59,9 +16,9 @@ class Grid {
   columns: {
     [col: number]: Marker[]
   } = {}
-  // minX = 0
-  // maxX = 0
-  // maxY = 0
+  minX: number | undefined
+  maxX: number | undefined
+  maxY = 0
 
   addRock(p1: Point, p2: Point | undefined) {
     if (p2 === undefined) {
@@ -69,6 +26,18 @@ class Grid {
     }
     const [xStart, xEnd] = p1.x < p2.x ? [p1.x, p2.x] : [p2.x, p1.x]
     const [yStart, yEnd] = p1.y < p2.y ? [p1.y, p2.y] : [p2.y, p1.y]
+
+    if (!this.minX || xStart < this.minX) {
+      this.minX = xStart
+    }
+
+    if (!this.maxX || xEnd > this.maxX) {
+      this.maxX = xEnd
+    }
+
+    if (yEnd + 2 > this.maxY) {
+      this.maxY = yEnd + 2
+    }
 
     for (let x = xStart; x <= xEnd; x++) {
       if (!this.columns[x]) {
@@ -108,7 +77,7 @@ class Grid {
 
     const output_arr: string[] = []
     for (let y = 0; y < y_end; y++) {
-      const curr_line: string[] = [y.toString().padStart(3, "0") + ":" ]
+      const curr_line: string[] = [y.toString().padStart(3, "0") + ":"]
       for (let x = x_start; x <= x_end; x++) {
         const point = !this.columns[x] ? "_" : this.columns[x][y] || "_"
         curr_line.push(point)
@@ -129,41 +98,38 @@ class Grid {
     }
 
     while (true) {
-      if (this.columns[x]?.length <= y) { return false }
-      const fall_to = this.columns[x].slice(y).findIndex((square) => square != ".") + y - 1
+      if (this.columns[x]?.length <= y) {
+        return false
+      }
+      const fall_to =
+        this.columns[x].slice(y).findIndex((square) => square != ".") + y - 1
       // There was no empty space below
       if (fall_to === -1) {
-        // console.log("Ahhhh! Falling forever!")
         return false
       }
 
       // If there is no column to the left, the sand will fall
       if (!this.columns[x - 1]) {
-        // console.log("nothing to the left")
         return false
       }
       const down_left = this.columns[x - 1][fall_to + 1]
       if (down_left === "." || down_left === undefined) {
-        // console.log("going down left")
         x = x - 1
         y = fall_to + 1
         continue
       }
 
       if (!this.columns[x + 1]) {
-        // console.log("nothing to the right")
         return false
       }
       const down_right = this.columns[x + 1][fall_to + 1]
       if (down_right === "." || down_right === undefined) {
-        // console.log("going down right")
         x = x + 1
         y = fall_to + 1
         continue
       }
 
       // If it gets here, it is resting
-      // console.log("resting:", fall_to, ",", y)
       this.drawSand(new Point(x, fall_to))
       return true
     }
@@ -197,26 +163,41 @@ const part1 = (rawInput: string) => {
   const grid = parseInput(rawInput)
   let count = 0
 
-  console.log(grid.toString())
-
   while (grid.dropSand(new Point(500, 0))) {
     count++
-    console.log("-----------------------\n")
-    console.log("Count:", count, "\n")
-    console.log(grid.toString())
   }
 
-  console.log("-----------------------\n")
-  console.log(grid.toString())
   return count
 }
 
 const part2 = (rawInput: string) => {
   const grid = parseInput(rawInput)
+  const floorY = grid.maxY
+
+  if (grid.minX === undefined || grid.maxX === undefined) {
+    throw new Error("min or max is undefined")
+  }
 
   let count = 0
 
-  return
+  while (true) {
+    try {
+      if (!grid.dropSand(new Point(500, 0))) {
+        // If sand falls off, add more floor
+        grid.addRock(
+          new Point(grid.minX - 10, floorY),
+          new Point(grid.maxX + 10, floorY),
+        )
+        continue
+      }
+    } catch {
+      return count
+    }
+    count++
+  }
+
+  // Should never reach here
+  return -1
 }
 
 run({
