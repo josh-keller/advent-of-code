2fbffba2233a16424b6ad1170f958f57f400f341
Josh Keller <jrkeller@gmail.com>
2022-12-20 08:56:13 -0500

    Part 2 working

--- 2022/README.md
+++ 2022/README.md
@@ -30,7 +30,7 @@
 [![Day](https://badgen.net/badge/17/%E2%98%85%E2%98%86/yellow)](src/day17)
 [![Day](https://badgen.net/badge/18/%E2%98%85%E2%98%85/green)](src/day18)
 ![Day](https://badgen.net/badge/19/%E2%98%86%E2%98%86/gray)
-[![Day](https://badgen.net/badge/20/%E2%98%85%E2%98%86/yellow)](src/day20)
+[![Day](https://badgen.net/badge/20/%E2%98%85%E2%98%85/green)](src/day20)
 ![Day](https://badgen.net/badge/21/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/22/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/23/%E2%98%86%E2%98%86/gray)
@@ -202,9 +202,9 @@ Both parts: -
 
 ```
 Day 20
-Time part 1: 70.721ms
-Time part 2: -
-Both parts: 70.720584ms
+Time part 1: 72.59ms
+Time part 2: 851.988ms
+Both parts: 924.578043ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 29/50
-Total time: 7704.211ms
+Total stars: 30/50
+Total time: 8558.068ms
 ```
 
 <!--/RESULTS-->
--- 2022/src/day20/index.ts
+++ 2022/src/day20/index.ts
@@ -26,9 +26,28 @@ const part1 = (rawInput: string) => {
 }
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput)
+  const KEY = 811589153
+  let numbers = parseInput(rawInput).map(elem => ({...elem, n: elem.n * KEY }))
+  const length = numbers.length
+  const mod = length - 1
 
-  return
+  for (let times = 0; times < 10; times++) {
+    for (let i = 0; i < length; i++) {
+      const idx = numbers.findIndex((n) => n.orig === i)
+      const [elem] = numbers.splice(idx, 1)
+      const newIdx = (((idx + elem.n) % mod) + mod) % mod
+
+      numbers = [...numbers.slice(0, newIdx), elem, ...numbers.slice(newIdx)]
+    }
+  }
+
+  const zeroIdx = numbers.findIndex((elem) => elem.n === 0)
+
+  return (
+    numbers[(zeroIdx + 1000) % length].n +
+    numbers[(zeroIdx + 2000) % length].n +
+    numbers[(zeroIdx + 3000) % length].n
+  )
 }
 
 run({
@@ -51,10 +70,18 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+1
+2
+-3
+3
+-2
+0
+4
+`,
+        expected: 1623178306,
+      },
     ],
     solution: part2,
   },
